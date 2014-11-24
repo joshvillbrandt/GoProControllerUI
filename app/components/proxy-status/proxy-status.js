db0952e27bd41731c5e84224a32ce084bbe2bcf0
Josh Villbrandt <josh@javconcepts.com>
2014-11-24 15:59:31 -0800

    Always re-evaluate proxy health, even when API is down

--- app/components/proxy-status/proxy-status.js
+++ app/components/proxy-status/proxy-status.js
@@ -3,23 +3,24 @@
 
 var module = angular.module('proxyStatus', ['angularMoment', 'goProController']);
 
-module.directive('proxyStatus', ['SyncedCameras',
-  function(SyncedCameras) {
+module.directive('proxyStatus', ['SyncedCameras', '$interval',
+  function(SyncedCameras, $interval) {
     return {
       restrict: 'E',
       scope: {},
       transclude: false,
       controller: function($scope, $element){
+        // state vars
+        $scope.cameras = SyncedCameras.items();
         $scope.status = 'proxy dead';
         var last_attempt;
         var ok_threshold = 20000; // ms
 
-        // watch for updates
-        $scope.cameras = SyncedCameras.items();
-        $scope.$watch('cameras', function(cameras){
+        // evaluate status
+        var promise = $interval(function(){
           // extract last_attempt time
-          for(var i = 0; i < cameras.length; i++) {
-            var attempt = new Date(cameras[i].last_attempt);
+          for(var i = 0; i < $scope.cameras.length; i++) {
+            var attempt = new Date($scope.cameras[i].last_attempt);
             if(last_attempt === undefined || attempt > last_attempt)
               last_attempt = attempt;
           }
@@ -30,9 +31,9 @@ module.directive('proxyStatus', ['SyncedCameras',
             var delta = (new Date()).getTime() - last_attempt.getTime();
             $scope.status = (delta <= ok_threshold) ? 'proxy ok' : 'proxy dead';
           }
-        }, true);
+        }, ok_threshold / 10);
       },
-      template: '<span class="label proxy-status" ng-class="{\'label-primary\': status==\'proxy ok\', \'label-danger\': status==\'proxy dead\'}"><span class="glyphicon" ng-class="{\'glyphicon-ok\': status==\'proxy ok\', \'glyphicon-exclamation-sign\': status==\'proxy dead\'}"></span>{{status}}</span>',
+      template: '<span class="label proxy-status" ng-class="{\'label-primary\': status==\'proxy ok\', \'label-danger\': status==\'proxy dead\'}"><span class="glyphicon" ng-class="{\'glyphicon-ok\': status==\'proxy ok\', \'glyphicon-exclamation-sign\': status==\'proxy dead\'}"></span>{{status==\'proxy ok\'?\'connected\':\'disconnected\'}}</span>',
       replace: true
     };
   }]);
