7c8958eb89a340c7156f3c9973a42ea0c6eb67ec
Josh Villbrandt <josh@javconcepts.com>
2014-12-05 01:17:10 -0800

    Start button group splitting

--- app/components/byte-breakdown/byte-breakdown.js
+++ app/components/byte-breakdown/byte-breakdown.js
@@ -9,7 +9,18 @@ module.directive('byteBreakdown', ['CameraConfig',
       restrict: 'E',
       transclude: false,
       controller: function($scope, $element){
+        // grab translation config
         $scope.config = CameraConfig.get();
+
+        // split bytes into logical groups
+        $scope.groups = [];
+        $scope.$watch('bytes', function(bytes){
+          var groups = [];
+          var pos = 0;
+          // while(pos < bytes.length) {
+          //   translation
+          // }
+        }, true);
       },
       templateUrl: 'components/byte-breakdown/byte-breakdown.html',
       replace: true
