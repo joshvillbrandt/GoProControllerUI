30b74f6a1d2a5294d1088e95c6bb39cc52ac5211
Josh Villbrandt <josh@javconcepts.com>
2014-12-05 15:38:48 -0800

    Debug page now shows byte groups (only for known byte groups)

--- app/components/byte-breakdown/byte-breakdown.js
+++ app/components/byte-breakdown/byte-breakdown.js
@@ -1,4 +1,4 @@
-(function(window, angular, undefined) {
+(function(window, _, angular, undefined) {
 'use strict';
 
 var module = angular.module('byteBreakdown', ['goProController']);
@@ -8,18 +8,52 @@ module.directive('byteBreakdown', ['CameraConfig',
     return {
       restrict: 'E',
       transclude: false,
+      scope: {
+        endpoint: '=',
+        bytes: '=',
+        status: '=',
+      },
       controller: function($scope, $element){
         // grab translation config
         $scope.config = CameraConfig.get();
 
-        // split bytes into logical groups
-        $scope.groups = [];
-        $scope.$watch('bytes', function(bytes){
-          var groups = [];
+        // determine byte groupings from configs
+        $scope.groupConfig = [];
+        $scope.$watch('config', function(config){
+          // make status config something searchable
+          var status = {};
+          _.forEach(config.status, function(value, key){
+            var maps = [];
+            _.forEach(value, function(mapping, name){
+              mapping.name = name;
+              maps.push(mapping);
+            });
+            status[key] = maps;
+          });
+
+          // now convert this into byte groups
           var pos = 0;
-          // while(pos < bytes.length) {
-          //   translation
-          // }
+          while(pos < 500) {
+            var mapping = _.find(status[$scope.endpoint], {a:pos});
+            // we found a group!
+            if(mapping !== undefined) {
+              $scope.groupConfig.push(mapping);
+              pos = mapping.b;
+            }
+            else {
+              pos = pos + 1;
+            }
+          }
+        }, true);
+
+        // split bytes into logical groups
+        $scope.$watch('bytes', function(bytes){
+          $scope.groups = [];
+          _.forEach($scope.groupConfig, function(config){
+            var group = _.cloneDeep(config);
+            group.bytes = bytes.substring(config.a, config.b);
+            $scope.groups.push(group);
+          });
         }, true);
       },
       templateUrl: 'components/byte-breakdown/byte-breakdown.html',
@@ -27,4 +61,4 @@ module.directive('byteBreakdown', ['CameraConfig',
     };
   }]);
 
-})(window, window.angular);
+})(window, window._, window.angular);
