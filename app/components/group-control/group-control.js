b156b4e0c95da4d8f9b0190f4ec1b403610099d9
Josh Villbrandt <josh@javconcepts.com>
2015-01-06 14:17:26 -0800

    Implement UI for sending camera commands

--- app/components/group-control/group-control.js
+++ app/components/group-control/group-control.js
@@ -1,15 +1,87 @@
 (function(window, angular, undefined) {
 'use strict';
 
-var module = angular.module('groupControl', []);
+var module = angular.module('groupControl', ['ui.bootstrap']);
 
-module.directive('groupControl', [
-  function() {
+module.directive('groupControl', ['CameraConfig', 'SyncedCameras', 'Commands',
+  function(CameraConfig, SyncedCameras, Commands) {
     return {
       restrict: 'E',
       transclude: false,
+      controller: function($scope, $element) {
+        // load command library
+        $scope.config = CameraConfig.get();
+
+        // load cameras
+        $scope.cameras = SyncedCameras.items();
+        window.cameras = $scope.cameras;
+
+        // submit camera commands
+        $scope.submit = function() {
+          // create new cmd
+          console.log($scope.targets, $scope.command, $scope.value)
+        };
+
+        // listen for camera select events from the camera list
+      },
       templateUrl: 'components/group-control/group-control.html',
       replace: false
+    };
+  }]);
+
+// originally from: http://jsfiddle.net/jaredwilli/vUSPu/
+module.directive('groupControlDropdown', [
+  function(){
+    return {
+      restrict: 'E',
+      scope:{
+        model: '=',
+        options: '='
+      },
+      templateUrl: 'components/group-control/group-control-dropdown.html',
+      replace: true,
+      controller: function($scope){
+        if($scope.model === undefined) $scope.model = [];
+
+        $scope.selectAll = function() {
+          $scope.model = _.pluck($scope.options, 'id');
+        };
+
+        $scope.deselectAll = function($event) {
+          $scope.model = [];
+
+          $event.stopPropagation();
+        };
+
+        $scope.setSelectedItem = function($event){
+          var id = this.option.id;
+
+          if(_.contains($scope.model, id)) {
+            $scope.model = _.without($scope.model, id);
+          }
+          else {
+            $scope.model.push(id);
+          }
+
+          $event.stopPropagation();
+        };
+
+        $scope.isChecked = function(id) {
+          if(_.contains($scope.model, id)) {
+            return 'selected';
+          }
+          return false;
+        };
+
+        $scope.modelNames = function() {
+          var names = [];
+          for(var i = 0; i < $scope.model.length; i++) {
+            var option = _.find($scope.options, {'id': $scope.model[i]});
+            names.push(option.ssid);
+          }
+          return names;
+        };
+      }
     };
   }]);
 
