aa26597aff7bdcc8d8c53896be92c8093c7d625b
Josh Villbrandt <josh@javconcepts.com>
2015-01-13 14:10:20 -0800

    Update version numbers

--- app/app.js
+++ app/app.js
@@ -24,7 +24,7 @@ var app = angular.module('app', [
   'page.debug',
 ]);
 
-app.value('version', '0.2.0');
+app.value('version', '0.2.2');
 app.value('api_root', 'http://localhost:8000');
 app.value('poll_rate', 1000); // ms
 
